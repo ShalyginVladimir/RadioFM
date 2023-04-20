fa8b1371a0ed076cd63f3fce53cdb3fc8b45ec1a
Vladimir Shalygin <vova.shalygin.88@mail.ru>
2023-04-20 21:52:20 +0300

    sign replacement

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -11,7 +11,7 @@ public class Radio {
     }
 
     public void prev() {
-        if (currentStation != 0) {
+        if (currentStation > 0) {
             currentStation--;
         } else {
             currentStation = 9;
